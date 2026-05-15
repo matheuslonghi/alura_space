0e252189bad1839e289ee3c415bb9513e201c05f
Matheus Longhi <matheuslonghiferreira@gmail.com>
2026-05-14 23:07:27 -0300

    aula Django: autenticação de formulários e alerta pt1

---
+++ usuarios/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, redirect
+from usuarios.forms import LoginForms, CadastroForms
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
+
+def login(request):
+    form = LoginForms()
+
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+
+        if form.is_valid():
+            nome = form["nome_login"].value()
+            senha = form["password"].value()
+
+            usuario = auth.authenticate(
+                request,
+                username=nome, 
+                password=senha
+            )
+            if usuario is not None:
+                auth.login(request, usuario)
+                messages.success(request, "Usuário logado com sucesso")
+                return redirect('index')
+            else:
+                messages.error(request, "Usuário e/ou senha incorretos")
+                return redirect('login')
+
+    return render(request, 'usuarios/login.html', {"form": form})
+
+def cadastro(request):
+    form = CadastroForms()
+
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+
+        if form.is_valid():
+            nome = form["nome_cadastro"].value()
+            email = form["email"].value()
+            senha = form["password1"].value()
+
+            if User.objects.filter(username=nome).exists():
+                messages.error(request, "Usuário já existente")
+                return redirect('cadastro')
+            
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            usuario.save()
+            messages.success(request, "Cadastro realizado com sucesso")
+            return redirect('login')
+        
+    return render(request, 'usuarios/cadastro.html', {"form": form})
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "Logout realizado com sucesso")
+    return redirect('login')
